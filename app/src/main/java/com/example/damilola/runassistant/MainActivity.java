a234cf8b3f64177d0f74694737d98b4837ff64b3
Rachel <afolabi6@uni.coventry.ac.uk>
2016-06-27 20:57:57 +0100

    third commit

--- app/src/main/java/com/example/damilola/runassistant/MainActivity.java
+++ app/src/main/java/com/example/damilola/runassistant/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.damilola.runassistant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button Login = (Button) findViewById(R.id.bLogin);
+        final Button SignUp = (Button) findViewById(R.id.bSignUp);
+
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent LoginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(LoginIntent);
+    }
+}
+
     }
 }
