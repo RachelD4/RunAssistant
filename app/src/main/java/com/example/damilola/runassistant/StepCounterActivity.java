8942199093d6ccbc9b5e10b8e65511e4d89ccea6
Rachel <afolabi6@uni.coventry.ac.uk>
2016-06-27 15:17:16 +0100

    Second commit

--- app/src/main/java/com/example/damilola/runassistant/StepCounterActivity.java
+++ app/src/main/java/com/example/damilola/runassistant/StepCounterActivity.java
@@ -1,13 +1,60 @@
 package com.example.damilola.runassistant;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StepCounterActivity extends AppCompatActivity {
+public class StepCounterActivity extends Activity implements SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_counter);
+
+        count = (TextView) findViewById(R.id.count);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume () {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor !=null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        activityRunning = false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (activityRunning){
+            count.setText(String.valueOf(event.values[0]));
+        }
+
     }
 }
